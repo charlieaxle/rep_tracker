d4fb2e195481fbd726cd2a4cbd9aea89c7d94cd3
charlieaxle <dillonmc26@gmail.com>
2019-01-01 22:40:00 +0000

    Added Initial Sets, Exercises, Gym models.

--- workouts/models.py
+++ workouts/models.py
@@ -6,4 +6,12 @@ class Gym(models.Model):
     gym_nm = models.CharField(max_length=200)
     rec_ins_ts = models.DateTimeField()
 
+class Exercise(models.Model):
+    exercise_nm = models.CharField(max_length=200)
+    ex_type_cd = models.CharField(max_length=3)
+    rec_ins_ts = models.DateTimeField()
+
+class Sets(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    reps = models.IntegerField(default=0)
 
